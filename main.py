1e0f8e806d3fc8e67c827f7263e0b631a980c210
Yashwanth Sai <68785131+theyashwanthsai@users.noreply.github.com>
2023-04-18 10:08:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ app = Ursina()
 ground = Entity(
     model='plane',
     texture='grass',
-    scale=(100, 1, 100),
+    scale=(1000, 1, 1000),
     collider='box'
 )
 # Create some walls
-wall1 = Entity(model='cube', scale=(1,10,100), position=(-50,1,0), texture='brick', collider='box')
-wall2 = Entity(model='cube', scale=(1,10,100), position=(50,1,0), texture='brick', collider='box')
-wall3 = Entity(model='cube', scale=(100,10,1), position=(0,1,-50), texture='brick', collider='box')
-wall4 = Entity(model='cube', scale=(100,10,1), position=(0,1,50), texture='brick', collider='box')
+wall1 = Entity(model='cube', scale=(1,100,1000), position=(-500,1,0), texture='brick', collider='box')
+wall2 = Entity(model='cube', scale=(1,100,1000), position=(500,1,0), texture='brick', collider='box')
+wall3 = Entity(model='cube', scale=(1000,100,1), position=(0,1,-500), texture='brick', collider='box')
+wall4 = Entity(model='cube', scale=(1000,100,1), position=(0,1,500), texture='brick', collider='box')
 # create a player entity with a weapon
 player = FirstPersonController(
     position=(0, 1, 0),
@@ -26,10 +26,13 @@ player = FirstPersonController(
     scale=(0.5, 1, 0.5),
     speed=10,
     jump_height=1,
-    gravity=1,
+    gravity=0,
     collider='box'
 )
 
+# Create Text objects for displaying sensor readings
+front_back_text = Text(text='Distance front/back: ', position=(-0.6, 0), origin=(0, 0), scale=1)
+left_right_text = Text(text='Distance left/right: ', position=(0, 0), origin=(0, 0), scale=1)
 
 
 # create a moving enemy entity
@@ -39,47 +42,84 @@ player = FirstPersonController(
 # left_threshold = 30
 # right_threshold = 30
 
-threshold = 50
+threshold = 20
 maxx = 120
-
+# thresholdspeed = 10
 
 def update():
+    global front_back_text, left_right_text
     if ser.in_waiting > 0:
         sensor_data = ser.readline().decode().strip().split(',')
         distance_front_back = int(sensor_data[0])
         distance_left_right = int(sensor_data[1])
+        
+        
+        
+        # Update Text objects with sensor readings
+        front_back_text.text = 'Distance front/back: ' + str(distance_front_back)
+        left_right_text.text = 'Distance left/right: ' + str(distance_left_right)
+        
+        
+        
+        
+        
         # player.x += 0
         # player.z += 0
         # Calculate movement based on sensor data
-        x_movement = 0
-        z_movement = 0
-        if distance_front_back == 0 and distance_left_right == 0:
+        if distance_front_back == 0 or distance_front_back > 170 and distance_left_right == 0:
             x_movement = 0
             z_movement = 0
-            
+            player.z += z_movement
+            player.x += x_movement
         else:
             
-            if distance_front_back < threshold and distance_front_back < maxx:
-                z_movement = 0.1
             
-            elif distance_front_back > threshold and distance_front_back < maxx:
-                z_movement = -0.1
             
-            elif distance_left_right < threshold and distance_left_right < maxx:
-                x_movement = -0.1
-
-            elif distance_left_right > threshold and distance_left_right < maxx:
-                x_movement = 0.1
-
+            if distance_front_back < 15 and distance_front_back < 80:
+                z_movement = 1
+                x_movement = 0
+                player.z += z_movement
+                player.x += x_movement 
+                
+            
+            elif distance_front_back > 15 and distance_front_back < 80:
+                z_movement = -1
+                x_movement = 0
+                player.z += z_movement
+                player.x += x_movement
+            
+            
+            
+            
+                
+       
+                
+                
+            elif distance_left_right < 15:
+                x_movement = -1
+                z_movement = 0
+                player.x += x_movement
+                player.z += z_movement
+                
+            
+                
+            elif distance_left_right > 15:
+                x_movement = 1
+                z_movement = 0
+                player.x += x_movement
+                player.z += z_movement
+                
             else:
                 x_movement = 0
                 z_movement = 0
+                player.z += z_movement
+                player.x += x_movement
             
         # Limit movement to avoid excessive speed
         # x_movement = max(min(x_movement, 0.1), -0.1)
         # z_movement = max(min(z_movement, 0.1), -0.1)
         
         # Move player
-        player.x += x_movement
-        player.z += z_movement
+        # player.x += x_movement
+        # player.z += z_movement
 app.run()
